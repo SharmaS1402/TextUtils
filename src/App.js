678e7381c5cba7411eee603a95cb878b69b8527b
SharmaS1402 <kusum9418177125@gmail.com>
2024-05-29 17:21:03 +0530

    improved dark mode and added two buttons

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import About from './components/About';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [mode, setMode] = useState('light');
+  function toggleMode(){
+    if(mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor = '#042743';
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+    }
+  }
   return (
     <>
-    <Navbar title="TextUtils" aboutText = "About"/>
+    <Navbar title="TextUtils" mode = {mode} toggleMode = {toggleMode}/>
     <div className="container my-3">
-    <TextForm heading = "Enter Text to analyze"/>
+    <TextForm heading = "Enter Text to analyze" mode = {mode}/>
     </div>
     {/* <About/> */}
     </>
